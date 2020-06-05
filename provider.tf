5b23b9197e293de20e4a8f4e1cc0dab8b3b8a984
jelinn <jelinn@gmail.com>
2020-06-05 14:29:37 -0500

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,13 +1,3 @@
 provider "aviatrix" {
   version = "~> 2.14"
 }
-
-provider "aws" {
-  region = "us-east-1"
-  alias  = "east1"
-}
-
-provider "aws" {
-  region = "us-east-2"
-  alias  = "east2"
-}
