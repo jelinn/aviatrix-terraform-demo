41a95f5e627b2b08f320dc7211a89e5d864a2a79
Abhishek Bhat <abhishekbhat@hotmail.com>
2020-06-10 10:40:15 -0700

    Controller update

--- main.tf
+++ main.tf
@@ -1,12 +1,12 @@
 module "aviatrix-create-avtx-vpcs-area1" {
   source  = "app.terraform.io/Aviatrix-TFC-JL/vpc-gw/aviatrix"
-  version = "0.0.2"
+  version = "0.0.4"
   cloud_type     = 1
   region         = var.aws_region1
   account_name   = var.aws_account_name
   spoke_gw_size  = var.aws_spoke_gw_size
   avx_transit_gw = module.aviatrix-create-transit-aws-area1.avtx_gw_name
-  vpc_count      = 2
+  vpc_count      = 4
 
   providers = {
     aws = aws.east1
@@ -15,13 +15,13 @@ module "aviatrix-create-avtx-vpcs-area1" {
 
 module "aviatrix-create-avtx-vnets-area1" {
   source  = "app.terraform.io/Aviatrix-TFC-JL/vpc-gw/aviatrix"
-  version = "0.0.2"
+  version = "0.0.4"
   cloud_type     = 8
   region         = var.azure_region1
   account_name   = var.azure_account_name
   spoke_gw_size  = var.azure_spoke_gw_size
   avx_transit_gw = module.aviatrix-create-transit-azure-area1.avtx_gw_name
-  vpc_count      = 2
+  vpc_count      = 4
 
   providers = {
     aws = aws.east1
@@ -30,7 +30,7 @@ module "aviatrix-create-avtx-vnets-area1" {
 
 module "aviatrix-create-transit-aws-area1" {
   source  = "app.terraform.io/Aviatrix-TFC-JL/transit-net/aviatrix"
-  version = "0.0.2"
+  version = "0.0.3"
   cloud_type     = 1
   region         = var.aws_region1
   account_name   = var.aws_account_name
@@ -43,7 +43,7 @@ module "aviatrix-create-transit-aws-area1" {
 
 module "aviatrix-create-transit-azure-area1" {
   source  = "app.terraform.io/Aviatrix-TFC-JL/transit-net/aviatrix"
-  version = "0.0.2"
+  version = "0.0.3"
   cloud_type     = 8
   region         = var.azure_region1
   account_name   = var.azure_account_name
@@ -57,15 +57,4 @@ module "aviatrix-create-transit-azure-area1" {
 resource "aviatrix_transit_gateway_peering" "test_transit_gateway_peering" {
   transit_gateway_name1 = module.aviatrix-create-transit-aws-area1.avtx_gw_name
   transit_gateway_name2 = module.aviatrix-create-transit-azure-area1.avtx_gw_name
-}
-
-resource "tls_private_key" "avtx_key" {
-  algorithm = "RSA"
-  rsa_bits  = 2048
-}
-
-resource "local_file" "avtx_priv_key" {
-  content         = tls_private_key.avtx_key.private_key_pem
-  filename        = "avtx_priv_key.pem"
-  file_permission = "0400"
 }
